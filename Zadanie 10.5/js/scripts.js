31143f660394ddcaf398757e4df71596afc21d2f
PiotrZieminski <piotrzieminski@outlook.com>
2017-03-12 15:35:27 +0100

    Dodane Zadanie 10.5

---
+++ Zadanie 10.5/js/scripts.js
@@ -0,0 +1,22 @@
+
+
+function button (text) {
+    this.text = text || 'Hello';
+}
+
+button.prototype = {
+    create: function () {
+        var self = this;
+        this.$element = $('<button>');
+        this.$element.text(this.text);
+        this.$element.click(function () {
+            alert(self.text);
+        })
+        $('body').append(this.$element);
+    }
+};
+
+var btn1 = new button('Hello');
+
+btn1.create();
+
